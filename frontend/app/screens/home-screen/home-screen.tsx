45f4dbbb475f8c170e606b1e7bc5d4d24a713ba2
Jeremy Wiens <jeremydwiens@gmail.com>
2020-10-19 16:22:44 -0700

    Added tabbed navigation in primary navigator and basic shells for home screen and profile screens

--- frontend/app/screens/home-screen/home-screen.tsx
+++ frontend/app/screens/home-screen/home-screen.tsx
@@ -1,23 +1,20 @@
 import React from "react"
 import { observer } from "mobx-react-lite"
 import { TextStyle, View, ViewStyle } from "react-native"
-import { Button, Header, Screen, Text } from "../../components"
+import { Screen } from "../../components"
 // import { useNavigation } from "@react-navigation/native"
-// import { useStores } from "../../models"
+import { useStores } from "../../models"
 import { color } from "../../theme"
-import auth from "@react-native-firebase/auth"
+import { Text } from "react-native-elements"
+import * as Progress from "react-native-progress"
+
+const progressWidth = 280
+const circleSize = 44
 
 const FULL: ViewStyle = { flex: 1 }
 const TEXT: TextStyle = {
   color: "black",
 }
-const HEADER_TEXT_STYLE: TextStyle = {
-  ...TEXT,
-  fontSize: 20,
-  lineHeight: 22,
-}
-
-const HEADER_STYLE: ViewStyle = {}
 
 const CONTENT: TextStyle = {
   ...TEXT,
@@ -26,20 +23,77 @@ const CONTENT: TextStyle = {
 const CONTENT_WRAP: ViewStyle = {
   flex: 1,
   alignItems: "center",
+}
+
+const MULTIPLER_STYLE: TextStyle = {
+  ...TEXT,
+  marginTop: 5,
+}
+
+const NEXT_MULT_STYLE: ViewStyle = {
+  alignItems: "flex-start",
+  width: progressWidth,
+  marginTop: 2,
+}
+
+const LEVEL_WRAP: ViewStyle = {
+  width: progressWidth,
+  marginTop: 20,
+}
+
+const MULTIPLIER_WRAP: ViewStyle = {
+  width: progressWidth,
+  overflow: "visible",
+}
+
+const LEVEL_STYLE: ViewStyle = {
+  width: circleSize,
+  height: circleSize,
+  borderRadius: 1000,
+  backgroundColor: "#008080",
+  alignItems: "center",
   justifyContent: "center",
 }
 
+const LEVEL_NUM_STYLE: TextStyle = {
+  fontSize: 30,
+}
+
+const PROGRESS_WRAP: ViewStyle = {
+  position: "absolute",
+  top: circleSize / 2 - 4, // minus height of progress bar
+}
+
+const LEVEL_NUM_WRAP: ViewStyle = {
+  position: "absolute",
+  right: 4,
+}
+
+const DAILY_GOAL_WRAP: ViewStyle = {
+
+}
+
+function getMultiplierColor(multiplier) {
+    if (multiplier < 0.2)
+      return "#008080";
+    if (multiplier < 0.5)
+      return "#00cd49"
+    if (multiplier < 0.8)
+      return "#e4e000"
+    else
+      return "#f20007"
+}
+
 export const HomeScreen = observer(function HomeScreen() {
-  async function signOut() {
-    __DEV__ && console.log("signing out")
-    try {
-      auth().signOut()
-    } catch (e) {
-      __DEV__ && console.log(e)
-    }
-  }
   // Pull in one of our MST stores
-  // const { someStore, anotherStore } = useStores()
+  const { userStore } = useStores()
+  const streakProgress = 0.8
+  const level = 7
+  const levelScore = 1248
+  const totalLevelScore = 2000
+  const levelProgress = levelScore / totalLevelScore;
+  const scoreMultiplier = 14
+
   // OR
   // const rootStore = useStores()
 
@@ -54,11 +108,31 @@ export const HomeScreen = observer(function HomeScreen() {
           style={HEADER_STYLE}
         /> */}
         <View style={CONTENT_WRAP}>
-          <Text style={CONTENT}>Hello World!</Text>
-          <Button
-            text="log out"
-            onPress={signOut}
-          ></Button>
+          <View style={LEVEL_WRAP}>
+            <View style={LEVEL_NUM_WRAP}>
+              <Text>
+                {levelScore} / {totalLevelScore}
+              </Text>
+            </View>
+            <View style={PROGRESS_WRAP}>
+              <Progress.Bar progress={levelProgress} width={progressWidth} color="#008080" />
+            </View>
+            <View style={LEVEL_STYLE}>
+              <Text style={LEVEL_NUM_STYLE}>{level}</Text>
+            </View>
+          </View>
+          <View style={MULTIPLIER_WRAP}>
+            <Text h4 style={MULTIPLER_STYLE}>
+              Score multiplier x{scoreMultiplier}
+            </Text>
+            <View style={NEXT_MULT_STYLE}>
+              <Text>Next multiplier</Text>
+            </View>
+            <Progress.Bar progress={streakProgress} width={progressWidth} color={getMultiplierColor(streakProgress)} />
+          </View>
+          <View style={DAILY_GOAL_WRAP}>
+            
+          </View>
         </View>
       </Screen>
     </View>
