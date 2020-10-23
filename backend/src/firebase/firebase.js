b14d938275034af9610837cd7dd8a88721929fd8
Harry Yao <harryyao13@gmail.com>
2020-10-23 11:04:28 -0700

    Cleanup

--- backend/src/firebase/firebase.js
+++ backend/src/firebase/firebase.js
@@ -7,33 +7,10 @@ class Firebase {
       credential: admin.credential.cert(serviceAccount),
       databaseURL: "https://dailydash-6d979.firebaseio.com"
     });
-    
-    // sendMessage();
+
   }
 
   // This registration token comes from the client FCM SDKs.
 }
 
-// function sendMessage() {
-//   var registrationToken = 'YOUR_REGISTRATION_TOKEN';
-
-//   var message = {
-//     data: {
-//       score: '850',
-//       time: '2:45'
-//     },
-//     token: registrationToken
-//   };
-
-//   // Send a message to the device corresponding to the provided
-//   // registration token.
-//   admin.messaging().send(message)
-//     .then((response) => {
-//       // Response is a message ID string.
-//       console.log('Successfully sent message:', response);
-//     })
-//     .catch((error) => {
-//       console.log('Error sending message:', error);
-//     });
-// }
 module.exports = new Firebase();
