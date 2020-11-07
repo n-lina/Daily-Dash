e443127501a71e3d51ab1f56f37ffdeaee168b07
n-lina <43121695+n-lina@users.noreply.github.com>
2020-11-06 23:56:38 -0800

    Update DetoxTest.java

--- frontend/android/app/src/androidTest/java/com/dailydash/DetoxTest.java
+++ frontend/android/app/src/androidTest/java/com/dailydash/DetoxTest.java
@@ -27,6 +27,5 @@ public class DetoxTest {
         detoxConfig.rnContextLoadTimeoutSec = (BuildConfig.DEBUG ? 180 : 60);
 
         Detox.runTests(mActivityRule, detoxConfig);
-        assertTrue(true);
     }
 }
