385ac198f00327df534440d7691b2c6c8be24a4d
Jeremy Wiens <jeremydwiens@gmail.com>
2020-11-13 20:13:21 -0800

    Remove duplicate 'setGoals'

--- frontend/app/models/daily-goal-store/daily-goal-store.ts
+++ frontend/app/models/daily-goal-store/daily-goal-store.ts
@@ -56,7 +56,6 @@ export const DailyGoalStoreModel = types
                 } else {
                   newGoals.push(self.updateGoal(goal, oldGoalsFiltered[0]));
                 }
-                self.setGoals(newGoals, day)
               });
               self.setGoals(newGoals, day)
             } else {
