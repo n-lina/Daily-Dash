0d7e0ef05e0ca09f2da767ee6c0b2d2ed1d38420
n-lina <43121695+n-lina@users.noreply.github.com>
2020-11-26 23:18:40 -0800

    Update welcome-screen.tsx

--- frontend/app/screens/welcome-screen/welcome-screen.tsx
+++ frontend/app/screens/welcome-screen/welcome-screen.tsx
@@ -27,7 +27,7 @@ const styles = StyleSheet.create({
   },
   image: {
     width: windowWidth,
-    height: windowWidth*1.2,
+    height: windowWidth,
     alignContent: "center", 
     alignItems: "center",
   }, 
